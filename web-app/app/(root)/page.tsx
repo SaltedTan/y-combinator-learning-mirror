cd6d26a16d76fe2e68d89068c8d21f4c908a305f
Isaac Leong <isaacleong23367@gmail.com>
2025-03-15 12:13:12 +1100

    Finished Half Font

--- web-app/app/(root)/page.tsx
+++ web-app/app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
         <>
-            HOME
+            <h1 className="uppercase bg-black px-6 py-3 font-worksans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+                HOME
+            </h1>
         </>
     );
 }
