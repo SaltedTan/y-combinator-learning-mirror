e67ee9a4cefdc82f605f9da5c508b3e7c7569c62
Isaac Leong <isaacleong23367@gmail.com>
2025-03-15 11:09:34 +1100

    Updated

---
+++ web-app/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from "next/image"
+import { auth, signOut, signIn } from '@/auth'
+
+// This is possible as this is a server component and use this async function
+const Navbar = async () => {
+    const session = await auth();
+    return (
+        <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                </Link>
+
+                <div className='flex items-center gap-5 text-black'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>
+                                    Create
+                                </span>
+                            </Link>
+
+                            <button onClick={signOut}>
+                                <span>Logout</span>
+                            </button>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name} </span>
+                            </Link>
+                        </>
+
+                    ) : (
+                        <button onClick={signIn('github')}>
+                            <span>
+                                Login
+                            </span>
+                        </button>
+                    )
+                    }
+
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
