88326b6e93e1a7b5f5e2555ea403a86cf65d3aa1
Sean Huan Zhi Tan <u7599095@anu.edu.au>
2025-03-22 23:08:50 +1100

    Implement all startups & startup card

--- web-app/app/(root)/page.tsx
+++ web-app/app/(root)/page.tsx
@@ -1,9 +1,22 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams } : {
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query;
+
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: { _id: 1, name: 'Adrian' },
+        _id: 1,
+        description: 'This is a description.',
+        image:
+            "https://unsplash.com/photos/a-glass-sculpture-of-a-man-and-a-woman-facing-each-other-rs-tSzJvpBs",
+        category: "Robots",
+        title: "We Robots",
+    }]
 
     return (
         <>
@@ -15,6 +28,22 @@ export default async function Home({ searchParams } : {
                 </p>
 
                 <SearchForm query={query} />
+            </section>
+
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : 'All Startups'}
+                </p>
+
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupCardType, index: number) => (
+                            <StartupCard key={post?.id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-result">No startups found</p>
+                    )}
+                </ul>
             </section>
         </>
     );
