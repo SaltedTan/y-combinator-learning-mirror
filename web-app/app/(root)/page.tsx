41373ef189a3669fb33da189d92aff0ee4b07ef2
Sean Huan Zhi Tan <u7599095@anu.edu.au>
2025-03-22 20:18:11 +1100

    Save changes before migrating to tailwindcss v4.0.15

--- web-app/app/(root)/page.tsx
+++ web-app/app/(root)/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="uppercase bg-black px-6 py-3 font-worksans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
-                HOME
-            </h1>
+            <h1 className="heading">Home</h1>
         </>
     );
 }
