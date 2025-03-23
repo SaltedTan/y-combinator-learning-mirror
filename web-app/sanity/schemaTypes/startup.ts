ab205e52c96fb78a9a7d2cd53ce5a71e968e1ed5
Sean Huan Zhi Tan <u7599095@anu.edu.au>
2025-03-23 14:51:28 +1100

    Implement startup details

--- web-app/sanity/schemaTypes/startup.ts
+++ web-app/sanity/schemaTypes/startup.ts
@@ -22,7 +22,7 @@ export const startup = defineType({
             to: { type: 'author' }
         }),
         defineField({
-            name: 'view',
+            name: 'views',
             type: 'number',
         }),
         defineField({
